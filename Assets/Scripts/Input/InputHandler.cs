ef24de0b32c57b29af4ffd4687fecc5f34a2582d
Constantinos Karaolis <kostaskaraolis@gmail.com>
2021-04-15 17:17:50 +0300

    removed easymobile. Added Mobile input

--- Assets/Scripts/Input/InputHandler.cs
+++ Assets/Scripts/Input/InputHandler.cs
@@ -8,15 +8,21 @@ namespace Foxlair.PlayerInput
         public Vector2 InputVector { get; private set; }
         public Vector3 MousePosition { get; private set; }
 
+        public Joystick Joystick;
+
         public bool IsFiringButtonDown;
         public bool IsInteractionButtonDown;
 
         public bool IsMovementButtonsDown;
 
+        private void Start()
+        {
+            Joystick = FindObjectOfType<Joystick>();
+        }
+
         void Update()
         {
             HandleMovementInput();
-            HandleWeaponInput();
             HandleGeneralCharacterInput();
 
             MousePosition = Input.mousePosition;
@@ -28,20 +34,33 @@ namespace Foxlair.PlayerInput
             IsInteractionButtonDown = Input.GetButton("Jump");
         }
 
-        private void HandleWeaponInput()
-        {
-            IsFiringButtonDown = Input.GetButton("Fire1");
-        }
-
         private void HandleMovementInput()
         {
              
             // in the future maybe allow half left of the screen to act as joy stick when you touch anywhere
-            var h = Input.GetAxisRaw("Horizontal");
-            var v = Input.GetAxisRaw("Vertical");
+            //var h = Input.GetAxisRaw("Horizontal");
+           // var v = Input.GetAxisRaw("Vertical");
 
+            var h = Joystick.Horizontal;
+            var v = Joystick.Vertical;
+      
+
+            //if (Joystick != null)
+            //{
+            //    if (h > 0 && v > 0)
+            //    {
+            //        Debug.Log($"Keyboard horizontal is: {h}. vertical is: {v}");
+            //    }
+            //    h = Joystick.Horizontal;
+            //    v = Joystick.Vertical;
+            //    if(Joystick.Horizontal > 0 && Joystick.Vertical > 0)
+            //    {
+            //        Debug.Log($"Joystick horizontal is: {Joystick.Horizontal}. vertical is: {Joystick.Vertical}");
+            //    }
+            //}
 
             InputVector = new Vector2(h, v);
+          
             IsMovementButtonsDown = (InputVector != Vector2.zero);
             //Debug.Log(inputVector);
         }
---
+++ Assets/Scripts/UIMobileInputHandling.cs
@@ -0,0 +1,35 @@
+﻿using Foxlair.PlayerInput;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMobileInputHandling : MonoBehaviour
+{
+
+    bool isFiringButtonPressed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onFireButtonPress()
+    {
+        isFiringButtonPressed=true;
+        InputHandler.Instance.IsFiringButtonDown = isFiringButtonPressed;
+
+    }
+
+    public void onFireButtonRelease()
+    {
+        isFiringButtonPressed=false;
+        InputHandler.Instance.IsFiringButtonDown = isFiringButtonPressed;
+
+    }
+}
