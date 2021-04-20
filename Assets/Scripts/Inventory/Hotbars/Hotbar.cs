b47c553c2812bcb931251c30a6419981a5eb9b56
Constantinos Karaolis <kostaskaraolis@gmail.com>
2021-04-20 17:45:34 +0300

    Part 7 - Hotbar Logic

---
+++ Assets/Scripts/Inventory/Hotbars/Hotbar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Foxlair.Inventory.Hotbars
+{
+    public class Hotbar : MonoBehaviour
+    {
+        [SerializeField] HotbarSlot[] hotbarSlots = new HotbarSlot[10];
+
+
+        public void Add(HotbarItem itemToAdd)
+        {
+            foreach(HotbarSlot hotbarSlot in hotbarSlots)
+            {
+                if (hotbarSlot.AddItem(itemToAdd)) { return; }
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/Inventory/Hotbars/HotbarItemDragHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Foxlair.Inventory.Hotbars
+{
+    public class HotbarItemDragHandler : ItemDragHandler
+    {
+        public override void OnPointerUp(PointerEventData eventData)
+        {
+            if(eventData.button== PointerEventData.InputButton.Left)
+            {
+                base.OnPointerUp(eventData);
+            
+                if(eventData.hovered.Count == 0)
+                {
+                    (ItemSlotUI as HotbarSlot).SlotItem = null;
+                }
+            }
+        }
+
+
+
+    }
+}
