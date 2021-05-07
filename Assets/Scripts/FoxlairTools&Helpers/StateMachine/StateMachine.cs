3febb434f500f48b558d096fe9b4a998bdd54b95
Constantinos Karaolis <kostaskaraolis@gmail.com>
2021-05-07 14:23:50 +0300

    Added change state with condition and fallback state in state machine

--- Assets/Scripts/FoxlairTools&Helpers/StateMachine/StateMachine.cs
+++ Assets/Scripts/FoxlairTools&Helpers/StateMachine/StateMachine.cs
@@ -8,10 +8,10 @@ namespace Foxlair.Tools.StateMachine
     public class StateMachine : MonoBehaviour
     {
 #if UNITY_EDITOR
-        [SerializeField] 
+        [SerializeField]
         private bool ShowDebugCurrentState = false;
 #endif
-        [SerializeField] 
+        [SerializeField]
         private State InitialState = null;
         public State[] AvailableStates;
 
@@ -54,13 +54,60 @@ namespace Foxlair.Tools.StateMachine
             CurrentState.OnStateEnter();
         }
 
+
+        /// <summary>
+        /// Change this state machine's current state. Pass in 'null' state to stop state machine.
+        /// If condition is not met change to fallbackState instead
+        /// </summary>
+        /// <param name="newState">New state to change to. 'null' to stop state machine. State must be a child to this state machine.</param>
+        public void ChangeState(State newState, State fallbackState, bool condition)
+        {
+            if (newState.StateMachine != this)
+            {
+#if UNITY_EDITOR
+                Debug.LogError($"Error occurred at <b>{gameObject.name}</b>: Not allowed to change state that doesn't belong to this state machine", this);
+#endif
+                return;
+            }
+
+            if (CurrentState != null)
+            {
+                CurrentState.OnStateExit();
+            }
+
+            if (condition)
+            {
+                CurrentState = newState;
+            }
+            else
+            {
+                CurrentState = fallbackState;
+            }
+
+            if (CurrentState == null) return;
+
+#if UNITY_EDITOR
+            if (ShowDebugCurrentState)
+            {
+                Debug.Log($"<b><color=#e74c3c>{gameObject.name}'s current state is </color><color=#f1c40f>{CurrentState.name}</color></b>", this);
+            }
+#endif
+
+            CurrentState.OnStateEnter();
+        }
+
+
+
+
+
         public virtual void Start()
         {
             if (CharacterStatesGameObject == null)
             {
                 AvailableStates = GetComponentsInChildren<State>();
             }
-            else {
+            else
+            {
                 AvailableStates = CharacterStatesGameObject.GetComponentsInChildren<State>();
             }
             for (int i = 0; i < AvailableStates.Length; i++)
